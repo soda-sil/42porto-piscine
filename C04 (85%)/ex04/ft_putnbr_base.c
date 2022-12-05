5c2dc74e20dc1eb172b8d8f69855f407e0a94614
Sofia Costa <113921409+soda-sil@users.noreply.github.com>
2022-12-05 18:51:00 +0000

    Add files via upload

---
+++ C04 (85%)/ex04/ft_putnbr_base.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soda-sil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 17:42:43 by soda-sil          #+#    #+#             */
+/*   Updated: 2022/10/06 18:19:50 by soda-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	finderror(char *str)
+{
+	int	i;
+	int	j;
+	int	x;
+
+	x = ft_strlen(str);
+	i = 0;
+	if (str[0] == '\0' || x == 1)
+		return (0);
+	while (str[i] != '\0')
+	{
+		if (str[i] == 43 || str[i] == 45 || str[i] <= 32 || str[i] == 127)
+			return (0);
+		j = i + 1;
+		while (j < ft_strlen(str))
+		{
+			if (str[i] == str[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	long	nb;
+	int		len;
+	int		error;
+
+	error = finderror(base);
+	len = ft_strlen(base);
+	nb = nbr;
+	if (error == 1)
+	{
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			nb *= -1;
+		}
+		if (nb < len)
+		{
+			ft_putchar(base[nb]);
+		}
+		if (nb >= len)
+		{
+			ft_putnbr_base(nb / len, base);
+			ft_putnbr_base(nb % len, base);
+		}
+	}
+}
+/*
+#include <stdio.h>
+int		main(void)
+{
+	ft_putnbr_base(-12, "01");
+	printf("\n");
+	ft_putnbr_base(40, "poneyvif");
+	printf("\n");
+	ft_putnbr_base(894867, "0123456789");
+	printf("\n");
+	ft_putnbr_base(53, "0123456789abcdef");
+}
+*/
