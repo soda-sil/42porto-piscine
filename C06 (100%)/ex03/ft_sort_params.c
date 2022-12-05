b1647093c491a524f4d1fd3a954a5f1cc5c7a73a
Sofia Costa <113921409+soda-sil@users.noreply.github.com>
2022-12-05 18:51:26 +0000

    Add files via upload

---
+++ C06 (100%)/ex03/ft_sort_params.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soda-sil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 18:18:33 by soda-sil          #+#    #+#             */
+/*   Updated: 2022/10/05 20:17:29 by soda-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else if (s1[i] < s2[i] || s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
+
+int	main(int argc, char **argv)
+{
+	int	j;
+	int	x;
+
+	x = 1;
+	while (x < argc - 1)
+	{
+		j = 1;
+		while (j < argc - 1)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				ft_swap(&argv[j], &argv[j + 1]);
+			}
+			j++;
+		}
+		x++;
+	}
+	j = 1;
+	while (j < argc)
+	{
+		ft_putstr(argv[j]);
+		write(1, "\n", 1);
+		j++;
+	}
+	return (0);
+}
